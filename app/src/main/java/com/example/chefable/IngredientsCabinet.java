6ab2806f515de708d4616f0c01bde824e1691e4f
Tyler Pease <40471000+SirTarragon@users.noreply.github.com>
2022-07-23 15:59:55 -0400

    more acitivities

---
+++ app/src/main/java/com/example/chefable/IngredientsCabinet.java
@@ -0,0 +1,14 @@
+package com.example.chefable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngredientsCabinet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredients_cabinet);
+    }
+}
