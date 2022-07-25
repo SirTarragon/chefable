75cb80546d64c7b3eecdd8e037a71e791d49a085
Tyler Pease <40471000+SirTarragon@users.noreply.github.com>
2022-07-25 16:36:06 -0400

    ContentProvider
    not too sure about how the Uri is being managed

--- app/src/main/java/com/example/chefable/MainActivity.java
+++ app/src/main/java/com/example/chefable/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.chefable;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    ListView itemsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        itemsList = (ListView) findViewById(R.id.ItemsList);
     }
 
     public void IngredientList_Intent(View view) {
@@ -26,10 +30,12 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void Search_Action(View view) {
+        itemsList.clearChoices();
 
     }
 
     public void AddFavorite_Action(View view) {
+        itemsList.getSelectedItem();
     }
 
     public void ViewFavorites_Intent(View view) {
