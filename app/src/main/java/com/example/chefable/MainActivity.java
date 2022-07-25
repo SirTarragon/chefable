1957807e24debe84cf5b5da7649035d6870f2692
Tyler Pease <40471000+SirTarragon@users.noreply.github.com>
2022-07-24 22:07:27 -0400

    some other stuff
    really distracted today

--- app/src/main/java/com/example/chefable/MainActivity.java
+++ app/src/main/java/com/example/chefable/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.chefable;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,27 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void IngredientList_Intent(View view) {
+        Intent intent = new Intent(this, IngredientsCabinet.class);
+        startActivity(intent);
+    }
+
+    public void Settings_Intent(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void Search_Action(View view) {
+
+    }
+
+    public void AddFavorite_Action(View view) {
+    }
+
+    public void ViewFavorites_Intent(View view) {
+        Intent inten = new Intent(this, Favorites.class);
+        startActivity(inten);
     }
 }
