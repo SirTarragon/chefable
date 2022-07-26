3cfd72b93de2c76b5192e35073035637382379f6
Tyler Pease <40471000+SirTarragon@users.noreply.github.com>
2022-07-25 22:01:10 -0400

    some work on web fragment

--- app/src/main/java/com/example/chefable/RecipeViewer.java
+++ app/src/main/java/com/example/chefable/RecipeViewer.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 
 public class RecipeViewer extends AppCompatActivity {
+    public static final String ARG_LINK = "link";
+    private static final String TAG_WEB_FRAGMENT = "web_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,17 @@ public class RecipeViewer extends AppCompatActivity {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction trans;
         trans = manager.beginTransaction();
+
+        WebFragment webFragment;
+        if (bundle != null) {
+            String url = bundle.getString(ARG_LINK);
+            webFragment = WebFragment.newInstance(url);
+        } else {
+            webFragment = WebFragment.newInstance();
+        }
+
+        trans.add(R.id.web_fragment_container, webFragment, TAG_WEB_FRAGMENT);
+        trans.commit();
     }
 
     public void Home_Intent(View view) {
