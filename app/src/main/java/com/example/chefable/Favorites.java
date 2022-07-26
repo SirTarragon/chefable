5141a1766dd8b6d97081faf04fa24727920af0d3
Tyler Pease <40471000+SirTarragon@users.noreply.github.com>
2022-07-25 22:50:47 -0400

    more activities xml

--- app/src/main/java/com/example/chefable/Favorites.java
+++ app/src/main/java/com/example/chefable/Favorites.java
@@ -2,7 +2,9 @@ package com.example.chefable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Favorites extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Favorites extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+    }
+
+    public void Home_Intent(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/chefable/IngredientsCabinet.java
+++ app/src/main/java/com/example/chefable/IngredientsCabinet.java
@@ -2,7 +2,9 @@ package com.example.chefable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IngredientsCabinet extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class IngredientsCabinet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients_cabinet);
+    }
+
+    public void Home_Intent(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
