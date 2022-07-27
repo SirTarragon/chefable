cbb559bdda954a43183d9132d2b6ae66f045f230
Tyler McLanahan <mclanahantyler@gmail.com>
2022-07-26 21:30:44 -0400

    Fixed client-side removal and webview functions

--- app/src/main/java/com/example/chefable/Favorites.java
+++ app/src/main/java/com/example/chefable/Favorites.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,6 +22,7 @@ public class Favorites extends AppCompatActivity {
     ArrayList<Recipe> favoritesArrayList;
     ListView favorites_list;
     ArrayAdapter<Recipe> arrayAdapter;
+    private Recipe selected_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +30,23 @@ public class Favorites extends AppCompatActivity {
         setContentView(R.layout.activity_favorites);
         favorites_list = findViewById(R.id.list_favorites);
         favoritesArrayList = new ArrayList<>();
+        Recipe test_recipe = new Recipe(1, "Pizza");
+        favoritesArrayList.add(test_recipe);
 
         Query query = FirebaseDatabase.getInstance().getReference().child("Favorites");
         arrayAdapter = new ArrayAdapter<>(Favorites.this,
                 android.R.layout.simple_list_item_1,
                 favoritesArrayList);
         favorites_list.setAdapter(arrayAdapter);
+
+        favorites_list.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                selected_item = (Recipe) favorites_list.getItemAtPosition(position);
+            }
+        });
 
         query.addValueEventListener(new ValueEventListener() {
             @Override
@@ -57,16 +70,17 @@ public class Favorites extends AppCompatActivity {
     }
 
     public void RemoveFav_Action(View view) {
-        Recipe item = (Recipe) favorites_list.getSelectedItem();
-        if (item != null) {
-            favoritesArrayList.remove(item);
-
+        if (selected_item != null) {
+            favoritesArrayList.remove(selected_item);
+            arrayAdapter.notifyDataSetChanged();
             Query query = FirebaseDatabase.getInstance().getReference().child("Favorites")
-                    .orderByChild("ID").equalTo(item.getID());
+                    .orderByChild("ID").equalTo(selected_item.getID());
             query.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    for(DataSnapshot removal : snapshot.getChildren()) {
+                public void onDataChange(@NonNull DataSnapshot snapshot)
+                {
+                    for(DataSnapshot removal : snapshot.getChildren())
+                    {
                         removal.getRef().removeValue();
 
                     }
@@ -81,10 +95,9 @@ public class Favorites extends AppCompatActivity {
     }
 
     public void ViewWeb_Intent(View view) {
-        Recipe item = (Recipe) favorites_list.getSelectedItem();
-        if (item != null) {
+        if (selected_item != null) {
             Bundle bundle = new Bundle();
-            bundle.putString("ARG_LINK", item.getLink().toString());
+            bundle.putString("ARG_LINK", selected_item.getLink().toString());
             Intent intent = new Intent(this, RecipeViewer.class);
             intent.putExtras(bundle);
             startActivity(intent);
